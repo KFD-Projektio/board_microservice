c3bb4a33d4d03ba8730405586bf6443d3ee12294
Cherkasov Ivan <142715333+tealover1337@users.noreply.github.com>
2025-05-01 16:04:41 +0300

    Removed ownerId field as the creator of board is automatically assigned as owner now

--- src/main/kotlin/ru/projektio/boardservice/dto/request/CreateBoardRequest.kt
+++ src/main/kotlin/ru/projektio/boardservice/dto/request/CreateBoardRequest.kt
@@ -3,6 +3,5 @@ package ru.projektio.boardservice.dto.request
 data class CreateBoardRequest(
     val title: String,
     val description: String?,
-    val ownerId: Long,
     val isPrivate: Boolean?
 )
