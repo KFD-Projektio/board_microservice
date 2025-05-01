643886d514ee901d693845bd8a54110bfce3ae32
Cherkasov Ivan <142715333+tealover1337@users.noreply.github.com>
2025-05-01 16:07:32 +0300

    Update ColumnService to use userId to perform checks, also refactored structure to make it a bit more readable

--- src/main/kotlin/ru/projektio/boardservice/service/ColumnService.kt
+++ src/main/kotlin/ru/projektio/boardservice/service/ColumnService.kt
@@ -9,20 +9,32 @@ import ru.projektio.boardservice.dto.request.CreateColumnRequest
 import ru.projektio.boardservice.dto.request.NewColumnTitleRequest
 import ru.projektio.boardservice.dto.response.ColumnDataResponse
 import ru.projektio.boardservice.exception.NoColumnException
+import ru.projektio.boardservice.exception.RestrictedUserException
 import ru.projektio.boardservice.exception.WrongColumnOrderException
 
 @Service
 class ColumnService(
     private val columnDao: ColumnDao,
-    private val columnMapper: ColumnMapper
+    private val columnMapper: ColumnMapper,
+    private val boardService: BoardService,
 ) {
 
-    fun getBoardColumns(boardId: Long): List<ColumnEntity> {
-        return columnDao.getColumnEntityByBoardId(boardId).toList()
+    fun getBoardColumns(userId: Long, boardId: Long): List<ColumnEntity> {
+        try { boardService.getBoardById(userId, boardId)
+            return columnDao.getColumnEntityByBoardId(boardId).toList()
+        }
+        catch (e: RestrictedUserException) {
+            throw RestrictedUserException("Can't check columns on board that you can't see")
+        }
     }
 
     @Transactional
-    fun addColumn(boardId: Long, data: CreateColumnRequest): ColumnDataResponse {
+    fun addColumn(userId: Long, boardId: Long, data: CreateColumnRequest): ColumnDataResponse {
+        try { boardService.getBoardById(userId, boardId) }
+        catch (e: RestrictedUserException) {
+            throw RestrictedUserException("Can't add columns on board that you can't see")
+        }
+
         val newColumn = ColumnEntity (
             columnTitle = data.title,
             boardId = boardId
@@ -32,24 +44,28 @@ class ColumnService(
         return columnMapper.columnData(newColumn)
     }
 
-    fun changeColumnTitle(boardId: Long, columnPosition: Int, data: NewColumnTitleRequest): ColumnDataResponse {
-        val boardColumns = getBoardColumns(boardId)
-        if (columnPosition > boardColumns.size || columnPosition < 0) {
-            throw NoColumnException("The postiton is wrong and/or the board doesn't have that many columns")
+    fun changeColumnTitle(userId: Long, boardId: Long, columnPosition: Int, data: NewColumnTitleRequest): ColumnDataResponse {
+        try {val boardColumns = getBoardColumns(userId, boardId)
+            if (columnPosition > boardColumns.size || columnPosition < 0) {
+                throw NoColumnException("The postiton is wrong and/or the board doesn't have that many columns")
+            }
+            val column = boardColumns[columnPosition]
+            column.columnTitle = data.title
+            columnDao.save(column)
+            return columnMapper.columnData(column)
         }
-        val column = boardColumns[columnPosition]
-        column.columnTitle = data.title
-        columnDao.save(column)
-        return columnMapper.columnData(column);
+        catch (e: RestrictedUserException) {throw RestrictedUserException("Can't change columns on board that you can't see")}
     }
 
-    fun reorderColumns(boardId: Long, newOrder: IntArray) {
+    fun reorderColumns(userId: Long, boardId: Long, newOrder: IntArray) {
+        try {boardService.getBoardById(userId, boardId)} catch (e: RestrictedUserException) {throw RestrictedUserException("Can't reorder columns on board that you can't see")}
         val columns = columnDao.getColumnEntityByBoardId(boardId)
         if (newOrder.size != columns.size) { throw WrongColumnOrderException("New positions aren't listed for each column")}
         for (i in 0..<columns.size) { columns[i].columnPosition = newOrder[i]; columnDao.save(columns[i])}
     }
 
-    fun deleteColumn(boardId: Long, columnPosition: Int) {
+    fun deleteColumn(userId: Long, boardId: Long, columnPosition: Int) {
+        try {boardService.getBoardById(userId, boardId)} catch (e: RestrictedUserException) {throw RestrictedUserException("Can't delete columns on board that you can't see")}
         columnDao.deleteColumnEntityByBoardIdAndColumnPosition(boardId, columnPosition)
     }
 }
